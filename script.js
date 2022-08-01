26bde029da3026eb61ceb810cb8ef65a3fefef7b
daldaniela <108768877+daldaniela@users.noreply.github.com>
2022-08-01 18:32:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var name= prompt("¿Cuál es tu nombre?");
+alert ("Bienvenido a El Buen Libro, " + name + "! Registrate y obtené un 10%OFF en tu primera compra :)" )
+
+function myalert() {
+    alert("Producto agregado a su carrito!");
+    }
